ea8912c6bef051e62892a8ebe9a9da842e3a2bb0
olivo4ka37 <kostylev.egor.1337@gmail.com>
2022-12-04 14:52:00 +0300

    Added a new directory helpers for usefull functions that will be used in a application, and added a Helpers.go in directory Helpers. Added ClientError function and ServerError function for giving errors report.
    Добавлена новая директория helpers для полезных функций, которые будут использоваться в приложении, в нем создан файл Helpers.go. В котором прописаны функции ClientError и ServerError для отчёта об ошибках разных типов.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/olivo4ka37/bookings/internal/config"
 	"github.com/olivo4ka37/bookings/internal/handlers"
+	"github.com/olivo4ka37/bookings/internal/helpers"
 	"github.com/olivo4ka37/bookings/internal/models"
 	"github.com/olivo4ka37/bookings/internal/render"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,6 +19,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -43,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -62,8 +72,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
